d539e7472797e92596257fb5b8eb3de7ea7411e2
Samuel Sampaio <samukasmk@gmail.com>
2025-11-26 03:25:30 -0300

    just create db file if not exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 set -e
 
+# define variables
 INSTANCE_PATH="${INSTANCE_PATH:-/app/instance}"
+DB_FILE="$INSTANCE_PATH/pokemon.db"
 
-# Ensure instance directory exists and is writable by api user
+# ensure instance directory exists and is writable by api user
 mkdir -p "$INSTANCE_PATH"
-touch  "$INSTANCE_PATH/pokemon.db"
+
+# create file if not exists
+if ! [ -e "$DB_FILE" ] ; then
+  touch "$DB_FILE"
+fi
+
+# fix owner to internal access inside container
 chown -R api:api "$INSTANCE_PATH"
+
+# allow permissions to external access
 chmod -R 0777 "$INSTANCE_PATH"
 
+# execute docker command with user: api (7777)
 exec gosu api "$@"
